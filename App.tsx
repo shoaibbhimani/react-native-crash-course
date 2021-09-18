73856e17d59af2963cc25350749882e41c317b10
shoaibbhimani <shoaibbhimani1392@gmail.com>
2021-09-19 03:14:39 +0530

    use-state-hook

--- App.tsx
+++ App.tsx
@@ -1,12 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{count}</Text>
+      <View style={styles.buttonContainer}>
+      <Button
+        title="Increment count"
+        onPress={() => {
+          setCount(count + 1);
+        }}
+      />
+
+      <Button
+        title="Decrement count"
+        onPress={() => {
+          setCount(count - 1);
+        }}
+      />
+      </View>
     </View>
   );
 }
@@ -14,8 +29,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  buttonContainer: {
+    flexDirection: 'row'
+  }
 });
